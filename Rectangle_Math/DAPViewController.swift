b4a8a66a59fb9d4c681e2605bf8dd676804725e3
Brandon Kimberlin <bkimberlin@humantorch.local>
2019-03-06 13:21:08 -0500

    Set up functions for perimeter and area. Set up button connections.

--- Rectangle_Math/DAPViewController.swift
+++ Rectangle_Math/DAPViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class DAPViewController: UIViewController {
 
+    @IBOutlet var LengthInput: UITextField!
+    @IBOutlet var WidthInput: UITextField!
+    @IBOutlet var ResultOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,38 @@ class DAPViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Diagonal(_ sender: UIButton)
+    {
+        
+    }
+    
+    @IBAction func Area(_ sender: UIButton)
+    {
+        if let length = LengthInput.text, let lengthVal = Double(length) {
+            if let width = WidthInput.text, let widthVal = Double(width) {
+                let result = lengthVal * widthVal
+                ResultOutput.text = String(result)
+            } else {
+                ResultOutput.text = "ERROR! No width value entered!"
+            }
+        } else {
+            ResultOutput.text = "ERROR! No length value entered!"
+        }
+    }
+    
+    @IBAction func Perimeter(_ sender: UIButton)
+    {
+        if let length = LengthInput.text, let lengthVal = Double(length) {
+            if let width = WidthInput.text, let widthVal = Double(width) {
+                let result = (lengthVal * 2) + (widthVal * 2)
+                ResultOutput.text = String(result)
+            } else {
+                ResultOutput.text = "ERROR! No width value entered!"
+            }
+        } else {
+            ResultOutput.text = "ERROR! No length value entered!"
+        }
+    }
 
 }
 
